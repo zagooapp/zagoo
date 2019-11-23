79d3c10c9efd59822b1e8b07b0c47f45050cddb1
mac <mac@macs-MacBook-Pro.local>
2019-11-23 20:08:54 +0200

    Updates

---
+++ precache-manifest.22d627813a7afc2bc32bee58e94ae3ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7a23a85aaecca723d5b0dd3cc00577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1249053f3caecf3c345",
+    "url": "/static/css/main.b0e7a66f.chunk.css"
+  },
+  {
+    "revision": "6fe33338d554f6c5cccb",
+    "url": "/static/js/2.40d0bec0.chunk.js"
+  },
+  {
+    "revision": "b1249053f3caecf3c345",
+    "url": "/static/js/main.46980bc3.chunk.js"
+  },
+  {
+    "revision": "5da240669dff55d578cd",
+    "url": "/static/js/runtime-main.65257217.js"
+  },
+  {
+    "revision": "574affe6c5d23f3c5dd0e686e13eb693",
+    "url": "/static/media/flex_studentsbg.574affe6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zagooapp/zagoo/precache-manifest.cb1af5c588acb7eced40bd8313f985e7.js"
+  "/precache-manifest.22d627813a7afc2bc32bee58e94ae3ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zagooapp/zagoo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
